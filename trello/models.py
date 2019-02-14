a2d7b4cde36baf61780c8f0957d1b925b62f3326
alsoncahyadi <alson.cahyadi@gmail.com>
2019-02-14 15:58:41 +0700

    Added models

---
+++ trello/models.py
@@ -0,0 +1,32 @@
+from django.db import models as m
+
+class Log(m.Model):
+    class Meta:
+        verbose_name = "Log"
+        verbose_name_plural = "Logs"
+  
+    def __str__(self):
+        super()
+
+    id = m.AutoField(verbose_name="Log ID", primary_key=True)
+
+class Complaint(m.Model):
+    class Meta:
+        verbose_name = "Complaint"
+        verbose_name_plural = "Complaints"
+  
+    def __str__(self):
+        super()
+
+    id = m.AutoField(verbose_name="Complaint ID", primary_key=True)
+    text = m.TextField(verbose_name="Complaint Text")
+    state = m.IntegerField(verbose_name="State")
+    category = m.CharField(verbose_name="Category" ,max_length = 10)
+    post_id = m.CharField(verbose_name="Instagram Post ID", max_length = 35)
+    comment_id = m.CharField(verbose_name="Instagram Post ID", max_length = 20)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    ready_at = models.DateTimeField()
+    wip_at = models.DateTimeField()
+    resolved_at = models.DateTimeField()
--- trello/urls.py
+++ trello/urls.py
@@ -1,4 +1,4 @@
-"""julid URL Configuration
+"""trello URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.1/topics/http/urls/
@@ -13,10 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.urls import include, path
 from django.contrib import admin
-from django.urls import path, include
+from rest_framework import routers
+from . import views as v
+import os
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('trello/', include('trello.urls'))
+    path('webhook/', v.Webhook.as_view())
 ]
