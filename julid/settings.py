66c44f79520481c45275d1a9236fabac193cb4ca
alsoncahyadi <alson.cahyadi@gmail.com>
2019-02-24 16:47:22 +0700

    Edited settings

--- julid/settings.py
+++ julid/settings.py
@@ -46,19 +46,19 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'allauth',
     'rest_framework',
     'rest_framework.authtoken',
     'rest_auth',
     'trel',
     'background_task',
-    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
+    # 'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
